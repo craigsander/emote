01ab3f3d6d2181983d33cfdd1045335e34147e00
Craig Sander <csander@centriole-solutions.com>
2019-02-18 13:56:45 -0500

    a

--- myproject/emote/admin.py
+++ myproject/emote/admin.py
@@ -5,19 +5,19 @@ from . import models
 
 class IncidentAdmin(admin.ModelAdmin):
     model = models.Incident
-    list_display = ['__all__', ]
+    list_display = ['created', 'incidentType', 'location']
 
 admin.site.register(models.Incident, IncidentAdmin)
 
 
 class CheckinAdmin(admin.ModelAdmin):
     model = models.Checkin
-    list_display = ['__all__', ]
+    list_display = ['created', 'feeling', 'intesity', 'location' ]
 
 admin.site.register(models.Checkin, CheckinAdmin)
 
 class QuoteAdmin(admin.ModelAdmin):
     model = models.Quote
-    list_display = ['__all__', ]
+    list_display = ['id', ]
 
 admin.site.register(models.Quote, QuoteAdmin)
